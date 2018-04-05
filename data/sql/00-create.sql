0355ea8ab7cbce261e2b47e27b6d6dce28164c78
Daniel Archer <contato@danielcramer.com.br>
2018-04-05 15:55:50 -0300

    update create sql

--- data/sql/00-create.sql
+++ data/sql/00-create.sql
@@ -17,6 +17,19 @@ CREATE TABLE `Containers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `Containers` (`id`, `hash`, `shortHash`, `name`, `image`, `memory`, `created`) VALUES
+(1, NULL, NULL, 'asdasdasd',  'alpine', '128M', '2018-04-05 14:20:49'),
+(2, NULL, NULL, 'asdasda',  'alpine', '128M', '2018-04-05 14:22:34'),
+(3, NULL, NULL, 'asdasda',  'alpine', '128M', '2018-04-05 14:22:54'),
+(4, NULL, NULL, 'asdasda',  'alpine', '128M', '2018-04-05 14:24:15'),
+(5, NULL, NULL, 'asdasdasd',  'alpine', '128M', '2018-04-05 14:52:02'),
+(6, NULL, NULL, 'asdasdas', 'alpine', '128M', '2018-04-05 14:53:36'),
+(7, NULL, NULL, 'teste',  'alpine', '128M', '2018-04-05 15:15:53'),
+(8, NULL, NULL, 'teste2', 'alpine', '128M', '2018-04-05 15:16:14'),
+(9, NULL, NULL, 'testeC', 'alpine', '128M', '2018-04-05 15:16:37'),
+(10,  NULL, NULL, 'testando', 'alpine', '128M', '2018-04-05 15:24:45'),
+(11,  NULL, NULL, 'testador2',  'alpine', '1G', '2018-04-05 18:51:21');
+
 DROP TABLE IF EXISTS `Hosts`;
 CREATE TABLE `Hosts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -27,7 +40,8 @@ CREATE TABLE `Hosts` (
 
 INSERT INTO `Hosts` (`id`, `address`, `name`) VALUES
 (1, 'host1:2375', 'Best First Host'),
-(2, 'host2:2375', 'Great Second Host');
+(2, 'host2:2375', 'Great Second Host'),
+(3, '10.7.8.50:2376', 'Chronos');
 
 DROP TABLE IF EXISTS `Networks`;
 CREATE TABLE `Networks` (
@@ -43,4 +57,12 @@ CREATE TABLE `Users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2018-04-05 12:23:08
+DROP TABLE IF EXISTS `Users_Containers`;
+CREATE TABLE `Users_Containers` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `containerId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2018-04-05 18:55:23
